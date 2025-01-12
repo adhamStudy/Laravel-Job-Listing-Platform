bd92f607844524b16ea997692be665a28cbe7cfa
adhamStudy <adhm000111000111@gmail.com>
2025-01-12 18:28:25 +0300

    Hero section done

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
      integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
      crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -13,7 +14,9 @@
 </head>
 <body class="bg-gray-100">
    <x-header/>
-    
+    @if(request()->is('/'))
+    <x-hero title="Find Your Dream Job" />
+    @endif
     <main>
        {{$slot}}
     </main>
